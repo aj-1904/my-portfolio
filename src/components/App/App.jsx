30c0e22994bae60587081a9a02acc27d2e0fbbd3
aj-1904 <jainayush827@gmail.com>
2021-07-06 22:03:10 +0530

    Understanding the Programmatic Navigation in React Router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,18 +7,20 @@ import Projects from "../Projects/Projects";
 import Skills from "../Skills/Skills";
 import Contact from "../Contact/Contact";
 import NavBar from "../NavBar/NavBar";
+import GoHome from "../GoHome/GoHome";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <div>
-            <NavBar />
+          <NavBar />
           <Route path="/" component={Home} exact></Route>
           <Route path="/about" component={About}></Route>
           <Route path="/projects" component={Projects}></Route>
           <Route path="/skills" component={Skills}></Route>
           <Route path="/contact" component={Contact}></Route>
+          <GoHome />
         </div>
       </BrowserRouter>
     </div>
