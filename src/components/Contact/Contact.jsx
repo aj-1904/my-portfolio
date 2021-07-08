99668ae8176f16ed2963cf1c4703fd93e601f995
aj-1904 <jainayush827@gmail.com>
2021-07-08 16:02:20 +0530

    Completing Contact Page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,8 +3,8 @@ import "./Contact.css";
 import Header from "../Header/Header";
 import FooterLink from "../FooterLink/FooterLink";
 import contactVector from "./../../assets/contact_anime.png";
-import github from "./../../assets/gh.png"
-import linkedin from "./../../assets/li.png"
+import github from "./../../assets/gh.png";
+import linkedin from "./../../assets/li.png";
 
 function Contact() {
   return (
@@ -13,13 +13,43 @@ function Contact() {
         heading="Get in touch."
         details="Feel free to drop me an email!"
       />
+      {/* form section */}
+      <div className="contact-form-container">
+        <form className="contact-form">
+          {/* email id input */}
+          <input
+            type="email"
+            placeholder="Your Email ID"
+            name="email"
+            className="input-box email-input"
+          />
+
+          {/* email body */}
+          <textarea
+            type="text"
+            placeholder="Your Message"
+            name="message"
+            className="input-box
+            body-input"
+          />
+
+          {/* submit button */}
+          <button type="submit" className="contact-btn">
+            Send Email
+          </button>
+        </form>
+      </div>
+
       {/* social icons */}
       <div className="social-icons-container">
         <a href="https://github.com/aj-1904" className="social-icon">
-        <img src={github} alt="social" />
+          <img src={github} alt="social" />
         </a>
-        <a href="https://www.linkedin.com/in/ayush-jain-190495/" className="social-icon">
-        <img src={linkedin} alt="social" />
+        <a
+          href="https://www.linkedin.com/in/ayush-jain-190495/"
+          className="social-icon"
+        >
+          <img src={linkedin} alt="social" />
         </a>
       </div>
       <FooterLink phrase="Read more " link="about me!" toAddress="/about" />
